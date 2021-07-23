a7bcbdf2775c24f4ecd085f40d6d829a182b772e
Alex Kharouk <akharoukpp@gmail.com>
2021-07-23 18:34:18 +0100

    fine little Calculator

---
+++ src/Calculator.svelte
@@ -0,0 +1,8 @@
+<script>
+  let a = 0,
+    b = 0;
+</script>
+
+<input bind:value={a} type="number" />
+<input bind:value={b} type="number" />
+<h5>Sum is {a + b}</h5>
--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,3 +1,16 @@
+<script>
+  export let subtitle = 'Be wary of the challenge.';
+  import Calculator from './Calculator.svelte';
+  let inputText = 0;
+</script>
+
 <div>
   <h2>Welcome to the Great British Sweat Quiz!</h2>
+  <h3>{subtitle}</h3>
+  <section>
+    <h4>Your salary is ${inputText}</h4>
+    <input bind:value={inputText} type="text" />
+    <hr />
+    <Calculator />
+  </section>
 </div>
